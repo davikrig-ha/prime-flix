2fe58c063bd52982a5011875d04db8730dcc6c78
davikrig-ha <daviluia2@gmail.com>
2022-05-25 17:00:23 -0300

    not found and loading page

---
+++ src/pages/error/index.js
@@ -0,0 +1,12 @@
+import './error.css'
+
+function Error(){
+    return(
+        <div className="not-found">
+            <h1>404</h1>
+            <h2>Página não encontrada</h2>
+        </div>
+    )
+}
+ 
+export default Error;
